230669b680efa41ceb4cae49129d62f7365cf479
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-05-08 17:08:50 -0300

    Add files via upload

---
+++ ExerciciosPHP/ex3/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <title>3 - Calculadora Simples</title>
+
+        <style>
+            * {
+                color: purple;
+            }
+        </style>
+    </head>
+    <body>
+        <center><h2>CalculaSimples</h2></center>
+        <hr/>
+        <div class="row justify-content-center row-cols-1 row-cols-md-2 mb-3 text-center">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sw">
+                    <div class="card-header py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="purple" class="bi bi-joystick" viewBox="0 3 16 16">
+                        <path d="M10 2a2 2 0 0 1-1.5 1.937v5.087c.863.083 1.5.377 1.5.726 0 .414-.895.75-2 .75s-2-.336-2-.75c0-.35.637-.643 1.5-.726V3.937A2 2 0 1 1 10 2"/>
+                        <path d="M0 9.665v1.717a1 1 0 0 0 .553.894l6.553 3.277a2 2 0 0 0 1.788 0l6.553-3.277a1 1 0 0 0 .553-.894V9.665c0-.1-.06-.19-.152-.23L9.5 6.715v.993l5.227 2.178a.125.125 0 0 1 .001.23l-5.94 2.546a2 2 0 0 1-1.576 0l-5.94-2.546a.125.125 0 0 1 .001-.23L6.5 7.708l-.013-.988L.152 9.435a.25.25 0 0 0-.152.23"/>
+                        </svg>&nbsp;&nbsp;<font style="font-size: 30px; color: purple;"><b>CalculaSimples!</b></font>
+                    </div>
+                    <div class="card-body">
+                        <form action="resultado.php" method="post">
+                            <label class="form-label">Digite um valor</label>
+                            <input class="form-control" type="number" name="number1" required placeholder="1" autofocus/><br>
+                            <label class="form-label">Escolha o tipo de operação + (para soma), - (para subtrair), / (para divisão) e * (para multiplação)</label>
+                            <input class="form-control" type="text" name="operacao" required placeholder="Digite um deses + - * /"/><br>
+                            <label class="form-label">Digite um valor</label>
+                            <input class="form-control" type="number" name="number2" required placeholder="1"/><br>
+                            <input type="submit" class="btn btn-outline-danger" value="Descobrir"/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    
+    </script>
+</html>
---
+++ ExerciciosPHP/ex5/resultado.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <title>Par ou Ímpar??</title>
+    </head>
+    <body>
+        <center><h2>Revelatrom</h2></center>
+        <hr/>
+        <div class="row justify-content-center row-cols-1 row-cols-md-2 mb-3 text-center">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sw">
+                    <div class="card-header py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="currentColor" class="bi bi-calculator" viewBox="0 3 16 16">
+                        <path d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                        <path d="M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+                        </svg>&nbsp;&nbsp;<font style="font-size: 30px;"><b>Revelatrom revela se um número é par ou ímpar</b></font>
+                    </div>
+                    <div class="card-body">
+                        <?php
+                            $number1 = $_POST['dia'];
+                            
+                            switch ($number1) {
+                                case 1:
+                                    echo'Domingo';
+                                    break;
+                                case 2:
+                                    echo'Segunda-Feria';
+                                    break;
+                                case 3:
+                                    echo'Terça-Feria';
+                                    break;
+                                case 4:
+                                    echo'Quarta-Feria';
+                                    break;
+                                case 5:
+                                    echo'Quinta-Feria';
+                                    break;
+                                case 6:
+                                    echo'Sexta-Feria';
+                                    break;
+                                case 7:
+                                    echo'Sábado';
+                                    break;
+                                default:
+                                    echo 'Valor inválido!';
+                                    break;
+                            }
+
+                        ?>
+                        <br><h7/><br/><a href='index.php' class='btn btn-outline-success' tabindex='-1' role='button' autofocus>VOLTAR</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
